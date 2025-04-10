c2e9690b6a6c4f185d66871a6fcfecba3ff146dc
Tang Nhat Huy Nguyen <112855653+Huy3301@users.noreply.github.com>
2025-04-10 15:45:19 +0930

    Create React project

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,25 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Dashboard from "./views/Dashboard";
-import Login from "./views/Login";
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Dashboard />} />
-        <Route path="/login" element={<Login />} />
-      </Routes>
-    </BrowserRouter>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
+
+export default App;
